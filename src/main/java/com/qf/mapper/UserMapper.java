6692d099e299e811ce52b71eb5dfc99471efedc5
Anxiaoyi <13197824935@163.com>
2019-07-19 21:32:53 +0800

    将SSM的项目提交到本地仓库！

---
+++ src/main/java/com/qf/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.qf.mapper;
+
+import com.qf.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
+
+public interface UserMapper {
+
+    //1. 根据用户名查询数据条数
+    Integer findCountByUsername(@Param("username")String username);
+
+
+    //2. 添加用户信息
+    Integer save(User user);
+
+
+    //3. 根据用户名查询用户信息.
+    User findByUsername(@Param("username")String username);
+
+}
